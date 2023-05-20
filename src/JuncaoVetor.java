00381f81caa592c35598af3b3b119022f8f6b349
Eliane Marion <pf1551@fiap.com.br>
2023-05-19 21:58:44 -0300

    leitura do vetorA

--- src/JuncaoVetor.java
+++ src/JuncaoVetor.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 
 public class JuncaoVetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner leitor = new Scanner(System.in);
 		int[] vetA = new int[6];
 		int[] vetB = new int[6];
+		int i;
+		for(i=0; i<6;i++) {
+			System.out.println("Digite o " + (i+1) +" valor para o vetorA: ");
+			vetA[i] = leitor.nextInt();
+		}
 	}
 
 }
